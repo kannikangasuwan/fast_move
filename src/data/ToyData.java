e8f614b4cf3d3fe8f9eeda9da859a7540d17eeb1
prog02 <prog02@CR4-2PC07>
2020-11-10 16:59:14 +0700

    Parcel

--- src/data/ToyData.java
+++ src/data/ToyData.java
@@ -1,7 +1,9 @@
 package data;
 
 import base.Bike;
+import base.Location;
 import base.Parcel;
+import base.ParcelStatus;
 import base.Person;
 
 public class ToyData {
@@ -21,8 +23,19 @@ public class ToyData {
     }
 
     public static Parcel[] getParcels() {
-        Parcel[] parcels = new Parcel[10];
-        // your code
+        //Parcel[] parcels = new Parcel[10];
+        Parcel[] parcels = new Parcel [7];
+        //String[] Parcels = {"Sticker","Boxer gifts","Wiggley Worm","trolls","Ribbon","Cube","Balloon","Springs Slinky","Jigsaw","Puzzle book"};
+        parcels[0] =  new Parcel(0.5, ParcelStatus.READY_TO_SHIP);
+        parcels[1] = new Parcel(1, ParcelStatus.SHIPPING, new Location(100,200));
+        parcels[2] = new Parcel(2, ParcelStatus.DELIVERED, new Location(300,500));
+        parcels[3] = new Parcel(0.5, ParcelStatus.READY_TO_SHIP);
+        parcels[4] = new Parcel(4, ParcelStatus.SHIPPING);
+        parcels[5] = new Parcel(1, ParcelStatus.READY_TO_SHIP);
+        parcels[6] = new Parcel(2, ParcelStatus.DELIVERED, new Location(400,500));
+        parcels[7] = new Parcel(6, ParcelStatus.READY_TO_SHIP, new Location(400,200));
+    //   parcels[8] = new Parcel(2.5 ParcelStatus.DELIVERED,new Location(200,100));
+        
 
         return parcels;
     }
